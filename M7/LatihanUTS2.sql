842ea89f0b0d6a97f45be5996ea124cf09fd74dc
Excel951 <abigailexcelsisdeo@gmail.com>
2022-10-28 13:10:57 +0700

    Undone

--- M7/LatihanUTS2.sql
+++ M7/LatihanUTS2.sql
@@ -6,55 +6,88 @@ BEGIN
 DECLARE
     CURSOR BATAS IS
         SELECT 
-            w.WAREHOUSE_NAME AS GUDANG,
             o.ORDER_ID,
             o.ORDER_DATE AS TANGGAL,
             o.STATUS AS STATUS,
+            p.PRODUCT_ID AS "ID PRODUK",
             p.PRODUCT_NAME AS "NAMA PRODUK",
             p.LIST_PRICE AS HARGA,
+            i.WAREHOUSE_ID AS "ID GUDANG",
+            w.WAREHOUSE_NAME AS NMGUDANG,
             i.QUANTITY AS "JUMLAH AWAL",
-            SUM(CASE 
-                WHEN o.STATUS LIKE 'Shipped' THEN oi.QUANTITY
-                ELSE 0
-                END
-            ) AS "JUMLAH KELUAR" ,
-            (i.QUANTITY - 
-                SUM(CASE 
-                    WHEN o.STATUS LIKE 'Shipped' THEN oi.QUANTITY
-                    ELSE 0
-                    END
-                )
-            ) AS "JUMLAH AKHIR"
+            oi.QUANTITY AS "JUMLAH KELUAR"
+            -- SUM(CASE 
+            --     WHEN o.STATUS LIKE 'Shipped' THEN oi.QUANTITY
+            --     ELSE 0
+            --     END
+            -- ) AS "JUMLAH KELUAR" ,
+            -- (i.QUANTITY - 
+            --     SUM(CASE 
+            --         WHEN o.STATUS LIKE 'Shipped' THEN oi.QUANTITY
+            --         ELSE 0
+            --         END
+            --     )
+            -- ) AS "JUMLAH AKHIR"
         FROM INVENTORIES i 
         JOIN ORDER_ITEMS oi ON oi.PRODUCT_ID = i.PRODUCT_ID
         JOIN ORDERS o ON o.ORDER_ID = oi.ORDER_ID 
         JOIN PRODUCTS p ON p.PRODUCT_ID = i.PRODUCT_ID 
         JOIN WAREHOUSES w ON w.WAREHOUSE_ID=i.WAREHOUSE_ID
         WHERE o.ORDER_DATE BETWEEN TO_DATE(TGLAWAL, 'dd-mm-yyyy') AND TO_DATE(TGLAKHIR, 'dd-mm-yyyy')
-        GROUP BY o.ORDER_DATE , p.PRODUCT_NAME , p.LIST_PRICE , i.QUANTITY , o.STATUS , p.PRODUCT_ID , o.ORDER_ID , W.WAREHOUSE_NAME
+        GROUP BY o.ORDER_DATE , p.PRODUCT_NAME , p.LIST_PRICE , i.QUANTITY , oi.QUANTITY ,
+                    o.STATUS , p.PRODUCT_ID , o.ORDER_ID , W.WAREHOUSE_NAME , i.WAREHOUSE_ID
         ORDER BY o.ORDER_DATE;
+
+    NO NUMBER;
+    QTYMASUK NUMBER;
+    QTYKELUAR NUMBER;
+    QTYTOTAL NUMBER;
+    TEMPPRODUCT NUMBER;
+    TEMPWARE NUMBER;
 
 BEGIN
 
-    FOR OUTPUT IN BATAS LOOP
+    FOR OUTPUT1 IN BATAS LOOP
+        NO:=0;
+        QTYMASUK:=0;
+        QTYKELUAR:=0;
+        QTYTOTAL:=0;
+        TEMPPRODUCT:=OUTPUT1."ID PRODUK";
+        TEMPWARE:=OUTPUT1."ID GUDANG";
 
-        DBMS_OUTPUT.PUT_LINE('============================');
-        DBMS_OUTPUT.PUT_LINE('TANGGAL: '||OUTPUT.TANGGAL);
-        DBMS_OUTPUT.PUT_LINE(' ');
-        DBMS_OUTPUT.PUT_LINE('GUDANG: '||OUTPUT.GUDANG);
-        DBMS_OUTPUT.PUT_LINE(' ');
-        DBMS_OUTPUT.PUT_LINE('STATUS ORDER: '||OUTPUT.STATUS);
-        DBMS_OUTPUT.PUT_LINE(' ');
-        DBMS_OUTPUT.PUT_LINE('NAMA PRODUK: '||OUTPUT."NAMA PRODUK");
-        DBMS_OUTPUT.PUT_LINE(' ');
-        DBMS_OUTPUT.PUT_LINE('HARGA: '||OUTPUT.HARGA);
-        DBMS_OUTPUT.PUT_LINE(' ');
-        DBMS_OUTPUT.PUT_LINE('JUMLAH AWAL: '||OUTPUT."JUMLAH AWAL");
-        DBMS_OUTPUT.PUT_LINE(' ');
-        DBMS_OUTPUT.PUT_LINE('JUMLAH KELUAR: '||OUTPUT."JUMLAH KELUAR");
-        DBMS_OUTPUT.PUT_LINE(' ');
-        DBMS_OUTPUT.PUT_LINE('JUMLAH AKHIR: '||OUTPUT."JUMLAH AKHIR");
-        DBMS_OUTPUT.PUT_LINE(' ');
+        DBMS_OUTPUT.PUT_LINE('Tanggal: '||OUTPUT1.TANGGAL);
+        DBMS_OUTPUT.PUT_LINE('Kode Produk: '||OUTPUT1."ID PRODUK");
+        DBMS_OUTPUT.PUT_LINE('=====================================');
+        DBMS_OUTPUT.PUT_LINE('No. | Deskripsi       | Jumlah');
+
+        FOR AWAL IN BATAS LOOP
+            IF TEMPPRODUCT = AWAL."ID PRODUK" THEN
+                NO:=NO+1;
+                DBMS_OUTPUT.PUT_LINE(NO||'. | Produk di gudang '||AWAL.NMGUDANG||' | '||AWAL."JUMLAH AWAL");
+                QTYMASUK:=AWAL."JUMLAH AWAL"+QTYMASUK;
+
+            END IF;
+
+        END LOOP;
+
+        FOR AKHIR IN BATAS LOOP
+            IF TEMPPRODUCT = AKHIR."ID PRODUK" THEN
+                NO:=NO+1;
+                DBMS_OUTPUT.PUT_LINE(NO||'. | Produk keluar (order_id: '||AKHIR.ORDER_ID||') | '||AKHIR."JUMLAH KELUAR");
+                QTYKELUAR:=AKHIR."JUMLAH KELUAR"+QTYKELUAR;
+
+            END IF;
+
+        END LOOP;
+
+        QTYTOTAL:=QTYMASUK-QTYKELUAR;
+        IF MOD(QTYTOTAL /*N2*/, ABS(QTYTOTAL) /*N1*/) = 1 THEN
+            DBMS_OUTPUT.PUT_LINE(OUTPUT1."NAMA PRODUK"||' dengan ID: '||OUTPUT1."ID PRODUK"||' tersisa '||QTYTOTAL);
+        ELSE IF MOD(QTYTOTAL /*N2*/, ABS(QTYTOTAL) /*N1*/) = -1 THEN
+            DBMS_OUTPUT.PUT_LINE(OUTPUT1."NAMA PRODUK"||' dengan ID: '||OUTPUT1."ID PRODUK"||' kurang '||QTYTOTAL);
+
+        END IF;
+        END IF;
 
     END LOOP;
 
---
+++ M7/a.sql
@@ -0,0 +1,147 @@
+SELECT 
+            -- o.ORDER_ID,
+            -- o.ORDER_DATE AS TANGGAL,
+            -- o.STATUS AS STATUS,
+            p.PRODUCT_ID,
+            p.PRODUCT_NAME AS "NAMA PRODUK",
+            i.WAREHOUSE_ID,
+            p.DESCRIPTION,
+            -- p.LIST_PRICE AS HARGA,
+            i.QUANTITY AS "JUMLAH AWAL"
+            -- SUM(CASE 
+            --     WHEN o.STATUS LIKE 'Shipped' THEN oi.QUANTITY
+            --     WHEN o.STATUS LIKE 'Pending' THEN 0
+            --     ELSE 0
+            --     END
+            -- ) AS "JUMLAH KELUAR" ,
+            -- (i.QUANTITY - 
+            --     SUM(CASE 
+            --         WHEN o.STATUS LIKE 'Shipped' THEN oi.QUANTITY
+            --         WHEN o.STATUS LIKE 'Pending' THEN 0
+            --         ELSE 0
+            --         END
+            --     )
+            -- ) AS "JUMLAH AKHIR"
+        FROM INVENTORIES i 
+        JOIN ORDER_ITEMS oi ON oi.PRODUCT_ID = i.PRODUCT_ID
+        JOIN ORDERS o ON o.ORDER_ID = oi.ORDER_ID 
+        JOIN PRODUCTS p ON p.PRODUCT_ID = i.PRODUCT_ID 
+        WHERE o.ORDER_DATE BETWEEN TO_DATE('1-1-2016', 'dd-mm-yyyy') AND TO_DATE('1-10-2016', 'dd-mm-yyyy');
+        -- GROUP BY o.ORDER_DATE , p.PRODUCT_NAME , p.LIST_PRICE , i.QUANTITY , o.STATUS , p.PRODUCT_ID , o.ORDER_ID
+        -- ORDER BY o.ORDER_DATE;
+
+
+
+
+DECLARE
+    NO NUMBER;
+    QTYMASUK NUMBER;
+    QTYKELUAR NUMBER;
+    QTYTOTAL NUMBER;
+    TEMPPRODUCT NUMBER;
+    TEMPDATE ORDERS.ORDER_DATE%TYPE;
+    CURSOR BATAS1 IS
+        SELECT
+            O.ORDER_DATE AS TANGGAL,
+            P.PRODUCT_ID AS "ID PRODUK",
+            P.PRODUCT_NAME AS "NAMA PRODUK"
+        FROM ORDERS O
+        JOIN ORDER_ITEMS OI ON OI.ORDER_ID=O.ORDER_ID
+        JOIN PRODUCTS P ON P.PRODUCT_ID=OI.PRODUCT_ID
+        WHERE O.ORDER_DATE BETWEEN TO_DATE('1-1-2016', 'dd-mm-yyyy') AND TO_DATE('1-10-2016', 'dd-mm-yyyy');
+
+    CURSOR BATAS2 IS
+        SELECT 
+            o.ORDER_ID ,
+            p.PRODUCT_ID AS "ID PRODUK",
+            p.PRODUCT_NAME AS "NAMA PRODUK",
+            p.LIST_PRICE AS HARGA,
+            i.WAREHOUSE_ID AS "ID GUDANG",
+            w.WAREHOUSE_NAME AS NMGUDANG,
+            i.QUANTITY AS "JUMLAH AWAL",
+            oi.QUANTITY AS "JUMLAH KELUAR"
+            -- SUM(CASE 
+            --     WHEN o.STATUS LIKE 'Shipped' THEN oi.QUANTITY
+            --     ELSE 0
+            --     END
+            -- ) AS "JUMLAH KELUAR" ,
+            -- (i.QUANTITY - 
+            --     SUM(CASE 
+            --         WHEN o.STATUS LIKE 'Shipped' THEN oi.QUANTITY
+            --         ELSE 0
+            --         END
+            --     )
+            -- ) AS "JUMLAH AKHIR"
+        FROM INVENTORIES i 
+        JOIN ORDER_ITEMS oi ON oi.PRODUCT_ID = i.PRODUCT_ID
+        JOIN ORDERS o ON o.ORDER_ID = oi.ORDER_ID 
+        JOIN PRODUCTS p ON p.PRODUCT_ID = i.PRODUCT_ID 
+        JOIN WAREHOUSES w ON w.WAREHOUSE_ID=i.WAREHOUSE_ID
+        WHERE o.ORDER_DATE BETWEEN TO_DATE('1-1-2016', 'dd-mm-yyyy') AND TO_DATE('1-10-2016', 'dd-mm-yyyy')
+        GROUP BY o.ORDER_DATE , p.PRODUCT_NAME , p.LIST_PRICE , i.QUANTITY , oi.QUANTITY ,
+                    p.PRODUCT_ID , W.WAREHOUSE_NAME , i.WAREHOUSE_ID , o.ORDER_ID
+        ORDER BY o.ORDER_DATE;
+
+    CURSOR BATAS3 IS
+        SELECT
+            O.ORDER_DATE AS TANGGAL,
+            OI.PRODUCT_ID AS "ID PRODUK",
+            OI.ORDER_ID AS IDORDER,
+            OI.QUANTITY AS "JUMLAH KELUAR"
+        FROM ORDER_ITEMS OI
+        JOIN ORDERS O ON O.ORDER_ID=OI.ORDER_ID
+        WHERE O.ORDER_DATE BETWEEN TO_DATE('1-1-2016', 'dd-mm-yyyy') AND TO_DATE('1-10-2016', 'dd-mm-yyyy');
+
+
+BEGIN
+
+    FOR OUTPUT1 IN BATAS1 LOOP
+        NO:=0;
+        QTYMASUK:=0;
+        QTYKELUAR:=0;
+        QTYTOTAL:=0;
+        TEMPPRODUCT:=OUTPUT1."ID PRODUK";
+        TEMPDATE:=OUTPUT1.TANGGAL;
+
+        DBMS_OUTPUT.PUT_LINE('Tanggal: '||OUTPUT1.TANGGAL);
+        DBMS_OUTPUT.PUT_LINE('Kode Produk: '||OUTPUT1."ID PRODUK");
+        DBMS_OUTPUT.PUT_LINE('Nama Produk: '||OUTPUT1."NAMA PRODUK");
+        DBMS_OUTPUT.PUT_LINE('=====================================');
+        DBMS_OUTPUT.PUT_LINE('No. | Deskripsi       | Jumlah');
+
+        FOR AWAL IN BATAS2 LOOP
+            IF TEMPPRODUCT = AWAL."ID PRODUK" THEN
+                NO:=NO+1;
+                DBMS_OUTPUT.PUT_LINE(NO||'. | Produk di gudang '||AWAL.NMGUDANG||' | '||AWAL."JUMLAH AWAL");
+                QTYMASUK:=AWAL."JUMLAH AWAL"+QTYMASUK;
+
+            END IF;
+
+        END LOOP;
+
+        FOR AKHIR IN BATAS3 LOOP
+            IF TEMPPRODUCT = AKHIR."ID PRODUK" AND TEMPDATE = AKHIR.TANGGAL THEN
+                NO:=NO+1;
+                DBMS_OUTPUT.PUT_LINE(NO||'. | Produk keluar (order_id: '||AKHIR.IDORDER||') | '||AKHIR."JUMLAH KELUAR");
+                QTYKELUAR:=AKHIR."JUMLAH KELUAR"+QTYKELUAR;
+
+            END IF;
+
+        END LOOP;
+
+        DBMS_OUTPUT.PUT_LINE('=====================================');
+
+        QTYTOTAL:=QTYMASUK-QTYKELUAR;
+        IF ABS(QTYTOTAL)-QTYTOTAL = 0 THEN
+            DBMS_OUTPUT.PUT_LINE(OUTPUT1."NAMA PRODUK"||' dengan ID: '||OUTPUT1."ID PRODUK"||' tersisa '||ABS(QTYTOTAL));
+        ELSE
+            DBMS_OUTPUT.PUT_LINE(OUTPUT1."NAMA PRODUK"||' dengan ID: '||OUTPUT1."ID PRODUK"||' kurang '||ABS(QTYTOTAL));
+
+        END IF;
+
+        DBMS_OUTPUT.PUT_LINE('=====================================');
+        DBMS_OUTPUT.PUT_LINE('=====================================');
+
+    END LOOP;
+
+END;
